68c92893bbc062b051812fb51432cd2aec0f99b5
sungho8 <dkdlah0830@naver.com>
2020-09-07 19:26:48 +0900

    Develop GoogleLogin with FireBase
    구글 로그인 개발
    파이어베이스 연동

---
+++ StikerDiaryProj/app/src/main/java/com/example/stickerdiary/Schedule.kt
@@ -0,0 +1,13 @@
+package com.example.stickerdiary
+
+import android.util.Log
+
+class Schedule(var Check : Boolean, var StartDay:String,var EndDay:String,
+               var StartTime : String, var EndTime : String, var Title : String,
+               var Category : String, var Content: String){
+    fun print(){
+        Log.e("MainActivity", StartDay + " ~ " + EndDay)
+    }
+}
+
+
